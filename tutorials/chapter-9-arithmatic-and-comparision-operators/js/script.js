49ac1451c4777b650e44e8d3e52dd5666ebc4d54
Uidhtml <uidhtml@gmail.com>
2025-09-14 03:15:04 +0530

    Added chapter 9

---
+++ tutorials/chapter-9-arithmatic-and-comparision-operators/js/script.js
@@ -0,0 +1,57 @@
+// console.log(2+1);
+// console.log(2-1);
+// console.log(1-2);
+// console.log(2*3);
+// console.log(4/2);
+// console.log(10/3);
+// console.log(10%3);
+
+// let x = 1;
+// //Pre incremet
+// console.log(++x);
+// //Post incremet
+// console.log(x++);
+// console.log(x)
+
+// let y = 1;
+// //Pre decrement
+// console.log(--y);
+// //Post decrement
+// console.log(y--);
+// console.log(y)
+
+
+
+
+
+//Compoarision operators
+// console.log(2 == 2);
+// console.log(2 != 2);
+// console.log(2 == '2');
+// console.log(2 != '2');
+
+// console.log(2 === 2);
+// console.log(2 !== 2);
+
+let x = 'Gautam';
+let y = 'Kumar';
+// console.log(x.length > y.length);
+// console.log(x.length < y.length);
+console.log(x.length >= y.length);
+console.log(x.length <= y.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
