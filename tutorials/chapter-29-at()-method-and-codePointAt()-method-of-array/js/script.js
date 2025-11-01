5bf02d42d31fa18ce546d36a15118d172befa6df
uidhtml <54753484+uidhtml@users.noreply.github.com>
2025-11-01 09:25:33 +0530

    Added chapter 29

---
+++ tutorials/chapter-29-at()-method-and-codePointAt()-method-of-array/js/script.js
@@ -0,0 +1,28 @@
+// UIDHTML youtube channel 🙏
+
+// Chapter - 29
+
+//Dummy Array
+const arr = [11, 34, 56, 80];
+
+//First element
+console.log(arr[0]);
+console.log(arr.at(0));
+
+//Last elemnt
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+//middle element
+console.log(arr.at(2));
+
+//Method chaining
+console.log(arr.at(-1).toString());
+
+//Usecade with string
+const str = "uidhtml";
+
+console.log(str.at(2));
+console.log(str.at(-1));
+console.log(str.at(-3));
